af63e85e4df3f9d81336602c6a34e3da237b69d9
farook <farookphuket@gmail.com>
2021-05-28 13:41:08 +0700

    update for manjaro easy setup 28 May 2021

--- SETUP_LAMP_SERVER/fileman.sh
+++ SETUP_LAMP_SERVER/fileman.sh
@@ -1,6 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
 
-# this will copy the file to the specifig 
+# this will copy the file to the specifig
 
 
 TODAY=$(date +"%Y-%m-%d_at_%H:%M:%S")
@@ -9,9 +9,9 @@ USER_NAME=$USER
 
 
 # make backup dir
-mkdir -p ~/Documents/Old_config 
+mkdir -p ~/Documents/Old_config
 BACKUP_DIR=~/Documents/Old_config
-BACKUP_PATH=$BACKUP_DIR 
+BACKUP_PATH=$BACKUP_DIR
 
 
 
@@ -20,31 +20,32 @@ if [[ -f /etc/hosts ]]; then
     cp /etc/hosts $BACKUP_PATH/hosts.old_$TODAY
     mv /etc/hosts /etc/hosts.old
 fi
-cp files_inc/hosts /etc/ 
+cp files_inc/hosts /etc/
 
 
 
-cp files_inc/phpmyadmin.conf /etc/httpd/conf/extra/ 
+cp files_inc/phpmyadmin.conf /etc/httpd/conf/extra/
 
 
 if [[ -f /etc/httpd/conf/httpd.conf ]]; then
-    cp /etc/httpd/conf/httpd.conf $BACKUP_PATH/httpd.conf.old_$TODAY 
+    cp /etc/httpd/conf/httpd.conf $BACKUP_PATH/httpd.conf.old_$TODAY
     mv /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.old
 fi
-cp files_inc/httpd.conf /etc/httpd/conf/ 
+cp files_inc/httpd.conf /etc/httpd/conf/
 
 
 
-if [[ -f /etc/php/php.ini ]]; then 
-    cp /etc/php/php.ini $BACKUP_PATH/php.ini.old_$TODAY 
-    mv /etc/php/php.ini /etc/php/php.ini.old 
+if [[ -f /etc/php/php.ini ]]; then
+    cp /etc/php/php.ini $BACKUP_PATH/php.ini.old_$TODAY
+    mv /etc/php/php.ini /etc/php/php.ini.old
 fi
 cp files_inc/php.ini /etc/php/
 
 
 # copy v-host
-if [[ -f /etc/httpd/conf/extra/httpd-vhosts.conf ]]; then 
-    cp /etc/httpd/conf/extra/ $BACKUP_PATH/httpd-vhosts.conf.old_$TODAY 
+if [[ -f /etc/httpd/conf/extra/httpd-vhosts.conf ]]; then
+    # backup the vhosts file
+    cp /etc/httpd/conf/extra/httpd-vhosts $BACKUP_PATH/httpd-vhosts.conf.old_$TODAY
     mv /etc/httpd/conf/extra/httpd-vhosts.conf /etc/httpd/conf/extra/httpd-vhosts.conf.old
 fi
 cp files_inc/httpd-vhosts.conf /etc/httpd/conf/extra/
