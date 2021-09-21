22cc94bb57d4ec464bb92573fa2283ce68856c17
farook <farookphuket@gmail.com>
2021-09-21 15:06:29 +0700

    edit program list

--- install_use_program/pamac_install.sh
+++ install_use_program/pamac_install.sh
@@ -1,15 +1,16 @@
-#!/bin/bash 
+#!/bin/bash
 
 
 
-pamac update 
+pamac update
 
 pamac build ocenaudio
 
-pamac build google-chrome 
+
+pamac build google-chrome
 
 # =============================================================================
-# date 28 Jul 2021 brave has removed from the main repo 
-# I've got an error on the last update just because of brave keyring so I 
+# date 28 Jul 2021 brave has removed from the main repo
+# I've got an error on the last update just because of brave keyring so I
 # remove brave then install brave-bin
 pamac install brave-bin
